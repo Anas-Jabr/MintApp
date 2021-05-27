c40220f7fad38f5ac6be1c4eb203040a8138c254
Anas Jabr <anas12460@gmail.com>
2021-05-27 10:51:42 +0300

    Initial Commit

---
+++ Mint Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mint Task
+//
+//  Created by Anas J on 5/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
